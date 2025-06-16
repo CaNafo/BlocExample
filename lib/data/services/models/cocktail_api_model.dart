ed4df180729c3a33b534fda73bd70438e5dee0b3
Dejan Cancar <dejan.cancar97@gmail.com>
2025-06-16 15:00:18 +0200

    added cocktail model

---
+++ lib/data/services/models/cocktail_api_model.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cocktail_api_model.g.dart';
+
+@JsonSerializable()
+class CocktailApiModel {
+  factory CocktailApiModelModel.fromJson(Map<String, dynamic> json) =>
+      _$CocktailApiModelFromJson(json);
+
+  const CocktailApiModel({
+    required this.idDrink,
+    required this.strDrink,
+    this.strImageSource,
+  });
+
+  final String idDrink;
+  final String strDrink;
+  final String? strImageSource;
+
+  Map<String, dynamic> toJson() => _$CocktailApiModelToJson(this);
+}
